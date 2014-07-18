ca47a350291d73c99d60e2fa42b72cacd0f9a88b
Hui Deng <tianma_25@163.com>
2014-07-18 15:00:35 +0800

    update tika demo

--- winter/src/main/java/com/hui/tika/TikaDemo.java
+++ winter/src/main/java/com/hui/tika/TikaDemo.java
@@ -1,27 +1,25 @@
 package com.hui.tika;
 
-import java.io.FileInputStream;
-import java.nio.file.Paths;
+import java.io.InputStream;
 
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.sax.BodyContentHandler;
-import org.junit.Test;
-
-
-
 
 public class TikaDemo {
 
-	@Test
-	public void testExt() throws Exception {
+	public static void main(String[] args) throws Exception {
 		AutoDetectParser parser = new AutoDetectParser();
-		BodyContentHandler handler = new BodyContentHandler(50);
+		BodyContentHandler handler = new BodyContentHandler(50000);
 
-		FileInputStream fis = new FileInputStream(Paths.get("E:\\tmp\\Tamale Server Refactor Investigation.docx").toFile());
+		TikaDemo demo = new TikaDemo();
+		InputStream fis = demo.getClass()
+				.getResourceAsStream("Tamale Server Refactor Investigation.docx");
 		Metadata  metadata = new Metadata ();
 		parser.parse(fis, handler, metadata);
 		
+		System.out.println(handler);
 		return;
 	}
+
 }
